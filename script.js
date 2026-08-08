89b77ef0261ae596acbea187134ecff74805b1d2
kastalco <kastalcowork@gmail.com>
2026-08-08 07:45:54 -0500

    incomplete robin modal interaction

--- script.js
+++ script.js
@@ -1,13 +1,30 @@
 
-const dialogToys = document.getElementById('dialogToys');
+const dialogSpider = document.getElementById('dialog__spiderman');
+
+const dialogRobin = document.getElementById('dialog__robin');
+
 const button1 = document.getElementById('button1');
+
+const button2 = document.getElementById('button2');
+
 const dialogCloseButton = document.querySelector('.dialog__close__button');
 
 button1.addEventListener('click', () => {
-    dialogToys.showModal();
+    dialogSpider.showModal();
 })
 
 dialogCloseButton.addEventListener('click', () => {
-    dialogToys.close();
+    dialogSpider.close();
 })
+
+
+
+button2.addEventListener('click', () => {
+    dialogRobin.showModal();
+})
+
+dialogCloseButton.addEventListener('click', () => {
+    dialogSpider.close();
+})
+
 
